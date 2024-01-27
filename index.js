324841c2058c0391dfde2863f68cf67e157d4e2e
Matuksa <matuxapankevich@gmail.com>
2024-01-28 00:29:46 +0300

    create reaction to welcome message

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Client, GatewayIntentBits, MessageType } = require('discord.js');
+const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 const config = require('./config.json');
 const token = config.token;
 
 
 bot.once('ready', () => {});
+
+bot.on('messageCreate', (msg) => {
+    if (msg.channelId === '1200495232085672016' && msg.type === MessageType.UserJoin) {
+        msg.react('🚗');
+    }
+});
 
 
 bot.login(token);
